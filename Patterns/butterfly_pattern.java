c55f1dedfa564e273233acb9f0911285bef32e81
SUJAL KUMAR <sujalkumars5873@gmail.com>
2026-05-19 16:48:36 +0530

    Add files via upload

---
+++ Patterns/butterfly_pattern.java
@@ -0,0 +1,34 @@
+public class butterfly_pattern {
+    public static void main(String[] args) {
+        int m = 4;
+        //upper part
+        for(int i = 1; i<=m; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j = 1; j<=(m-i)*2; j++){
+                System.out.print(".");
+            }
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();    
+        }
+
+        //lower part
+
+        for(int i = m; i>=1; i--){
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j = 1; j<=(m-i)*2; j++){
+                System.out.print(".");
+            }
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
