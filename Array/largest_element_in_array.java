01d7900984435a621ebe6ba6ceb64044d3308c0f
SUJAL KUMAR <sujalkumars5873@gmail.com>
2026-05-20 14:30:46 +0530

    Add files via upload

---
+++ Array/largest_element_in_array.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class largest_element_in_array {
+    public static void main(String[] args) {
+        System.out.println("Enter total no. of index of array: ");
+        Scanner sc = new Scanner(System.in);
+        int index = sc.nextInt();
+
+        int arr[] = new int[index];//declearing array
+
+        System.out.println("enter values: ");
+        
+        //input
+        for (int i = 0; i < index; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = arr[0];//letting largest value be in index no.1
+
+        //output
+        for(int i = 0; i<index; i++){
+           if(arr[i]>max){
+            max = arr[i];
+           }
+        }
+        System.out.println("largest value is : " + max);
+    }
+    
+}
