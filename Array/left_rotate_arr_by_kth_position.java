8cb1435d8b5568c1f2ace6d6aff29b09389af51c
SUJAL KUMAR <sujalkumars5873@gmail.com>
2026-05-24 21:32:21 +0530

    Add files via upload

---
+++ Array/left_rotate_arr_by_kth_position.java
@@ -0,0 +1,35 @@
+public class left_rotate_arr_by_kth_position {
+
+
+    public static void rotate(int nums[],int d){
+         
+        d = d % nums.length;       //reduced the rotation under arr length
+
+        int temp[] = new int[d];    //deleared temp arr
+
+        for(int i = 0; i<d; i++){    //storing elements in temp arr
+            temp[i] = nums[i];
+        }
+
+         for(int i = d; i<nums.length; i++){         //shifting elements to right  or rotating left
+            nums[i-d] = nums[i];
+         }
+
+         for(int i = nums.length - d; i<nums.length; i++){
+            nums[i] = temp[i - (nums.length - d)];
+         }
+
+
+
+    }
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,4,5,6,7};
+        int d = 3;
+
+        rotate(nums, d);
+
+        for(int i = 0; i<nums.length; i++){
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
