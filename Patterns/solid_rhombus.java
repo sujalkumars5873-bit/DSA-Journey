bbdc2d8c12e732d48095d7450fc4d2210ac66c87
SUJAL KUMAR <sujalkumars5873@gmail.com>
2026-05-25 21:00:18 +0530

    Add files via upload

---
+++ Patterns/solid_rhombus.java
@@ -0,0 +1,15 @@
+public class solid_rhombus {
+    public static void main(String[] args) {
+        int m = 5;
+        for(int i = 1; i<=m; i++){
+            for(int j = 1; j<=m-i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j<=m; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
