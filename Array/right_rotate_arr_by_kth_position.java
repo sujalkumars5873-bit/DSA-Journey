7f9749a35e10b2c000e569b79210155318fb8288
SUJAL KUMAR <sujalkumars5873@gmail.com>
2026-05-27 16:10:39 +0530

    Update right_rotate_arr_by_kth_position.java

--- Array/right_rotate_arr_by_kth_position.java
+++ Array/right_rotate_arr_by_kth_position.java
@@ -2,19 +2,20 @@ public class right_rotate_arr_by_kth_position {
 
     public static void rotate(int nums[],int d){
          int n = nums.length;
-        d = d % n;       //reduced the rotation under arr length
+        
+            k = k % n;       //reduced the rotation under arr length
 
-        int temp[] = new int[d];    //deleared temp arr
+        int temp[] = new int[k];    //deleared temp arr
 
-        for(int i = 0; i<d; i++){    //storing elements in temp arr
-            temp[i] = nums[n-d+i];
+        for(int i = 0; i<k; i++){    //storing elements in temp arr
+            temp[i] = nums[n-k+i];
         }
 
-         for(int i = n-d-1; i>=0; i--){         //shifting elements to left  or rotating rotating
-            nums[i+d] = nums[i];
+         for(int i = n-k-1; i>=0; i--){         //shifting elements to right  
+            nums[i+k] = nums[i];
          }
 
-         for(int i = 0; i<d; i++){
+         for(int i = 0; i<k; i++){
             nums[i] = temp[i];
          }
 
@@ -23,9 +24,9 @@ public class right_rotate_arr_by_kth_position {
     }
     public static void main(String[] args) {
         int nums[] = {1,2,3,4,5,6,7};
-        int d = 3;
+        int k = 3;
 
-        rotate(nums, d);
+        rotate(nums, k);
 
         for(int i = 0; i<nums.length; i++){
             System.out.print(nums[i] + " ");
