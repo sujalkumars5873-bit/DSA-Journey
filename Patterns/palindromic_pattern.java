e85b33e76cbad81013c787fd71369b5b1a177884
SUJAL KUMAR <sujalkumars5873@gmail.com>
2026-05-27 17:35:13 +0530

    Add files via upload

---
+++ Patterns/palindromic_pattern.java
@@ -0,0 +1,18 @@
+public class palindromic_pattern {
+    public static void main(String[] args) {
+        int m = 5;
+        for(int i = 1; i<=m; i++){
+            for(int j = 1; j<=m-i; j++){
+                System.out.print(" ");
+            }
+            for(int j = i; j>=1; j--){
+                System.out.print(j);
+            }
+            for(int j = 2; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    
+}
