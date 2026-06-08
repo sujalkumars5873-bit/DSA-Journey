3608e5b989f9fac15bc661f7764841308b6597f8
SUJAL KUMAR <sujalkumars5873@gmail.com>
2026-06-08 19:51:39 +0530

    Add files via upload

---
+++ Array/max_consecutive_ones.java
@@ -0,0 +1,22 @@
+public class max_consecutive_ones {
+    public static void main(String[] args) {
+        
+
+        int arr[] ={1,1,0,1,1,1,0,1,1};
+        int cnt = 0;
+        int max = 0;
+        int n =arr.length;
+
+
+        for(int i = 0; i<n; i++){
+            if(arr[i] == 1){
+                cnt++;
+                max = Math.max(max,cnt);
+            }
+            else{
+                cnt = 0; 
+            }
+        }
+        System.out.println("max consecutive ones is: " + max);
+    }
+}
