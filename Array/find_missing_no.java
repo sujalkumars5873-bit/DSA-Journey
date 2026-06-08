528c2220b5bf94d44a1f335140d1e4fec3001de1
SUJAL KUMAR <sujalkumars5873@gmail.com>
2026-06-08 19:30:12 +0530

    Add files via upload

---
+++ Array/find_missing_no.java
@@ -0,0 +1,37 @@
+public class find_missing_no {
+    public static void main(String[] args) {
+        
+        int arr[] = {1,2,4,5};
+        
+        int n = 5;
+        int a = arr.length;
+                                                     //from linear search
+        
+        // for(int i = 1; i<n; i++){
+        //     int flag = 0;
+        //     for(int j = 0; j<n-1; j++){
+        //         if(arr[j] == i){
+        //              flag = 1;
+        //             break;
+        //         }
+        //     }
+        //     if (flag == 0) {
+        //         System.out.println(i);
+        //     }
+        // }
+
+
+                                                     //from summing all no.s
+
+        int sum1 = n*(n+1)/2;  //sum of all no upto n.
+        int sum2 = 0;
+        int missingNo;
+
+        for(int i = 0; i<a; i++){
+            sum2 = sum2 + arr[i];
+        }
+
+        missingNo = sum1 - sum2;
+        System.out.println("The missing no. in array is: " + missingNo);
+    }
+}
