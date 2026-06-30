92cd1d5911d3b4c9b0dd2e242a98566119a09e9a
SUJAL KUMAR <sujalkumars5873@gmail.com>
2026-06-30 09:27:33 +0530

    Update count_no.java

--- Maths problem/count_no.java
+++ Maths problem/count_no.java
@@ -3,7 +3,8 @@ public class count_no {
         int num = 778989;
         int count = 0;
         System.out.println("number: " + num);
-        while(num>0){
+        while(num>0)
+            int lastdigit = num % 10;        ///it is the last digit of the num given //
             count = count+1;
             num = num / 10;
         }
