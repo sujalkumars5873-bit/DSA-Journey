5db85e8777fa3ad8aaf7dcb9d9596e77bc2c2cca
SUJAL KUMAR <sujalkumars5873@gmail.com>
2026-06-30 20:50:23 +0530

    Update print_all_the_divisors.java

--- Maths problem/print_all_the_divisors.java
+++ Maths problem/print_all_the_divisors.java
@@ -7,5 +7,16 @@ public class print_all_the_divisors {
                 System.out.println(i);
             }
         }
+
+                //or
+        for(int i = 1; i*i <= n; i++){                           //here the i*i is the replacement of sqrroot of i... means instead of i*i, we can all write i<=sqrot i.
+            if(n % i == 0){
+                System.out.println(i);
+            if((n / i) != i){
+                System.out.println(n / i);
+            }
+        
+        }
+    }
     }
 }
