fb86042bc5d8a305335a74c71f3c3e8794396483
SUJAL KUMAR <sujalkumars5873@gmail.com>
2026-08-04 23:22:56 +0530

    Update Kadanes_algo.java

--- Array/Medium/Kadanes_algo.java
+++ Array/Medium/Kadanes_algo.java
@@ -1,32 +1,35 @@
 public class Kadanes_algo {
-    public static void main(String[] args) {
-        int nums[] = {2, 3, 5, -2, 7, -4};
 
-        int n = nums.length;
+                            //Brute Force approch//
 
-        int max = Integer.MIN_VALUE;
-        int start = 0;
-        int end = 0;
+//     public static void main(String[] args) {
+//         int nums[] = {2, 3, 5, -2, 7, -4};
 
-        for(int i = 0; i<n; i++){
+//         int n = nums.length;
 
-            for(int j = i; j<n; j++){
+//         int max = Integer.MIN_VALUE;
+//         int start = 0;
+//         int end = 0;
 
-                int sum = 0;
-                for(int k = i; k<=j; k++){
-                    sum += nums[k];
-                }
-                if(sum > max){
-                max = sum;
-                start = i;
-                end = j;
-            }
-            }
-        }
-        System.out.println(max);
-        System.out.print("subarray: " );
-        for(int b = start; b<= end; b++){
-            System.out.print(nums[b] + " ");
-        }
-    }
-}
+//         for(int i = 0; i<n; i++){
+
+//             for(int j = i; j<n; j++){
+
+//                 int sum = 0;
+//                 for(int k = i; k<=j; k++){
+//                     sum += nums[k];
+//                 }
+//                 if(sum > max){
+//                 max = sum;
+//                 start = i;
+//                 end = j;
+//             }
+//             }
+//         }
+//         System.out.println(max);
+//         System.out.print("subarray: " );
+//         for(int b = start; b<= end; b++){
+//             System.out.print(nums[b] + " ");
+//         }
+//     }
+// }
